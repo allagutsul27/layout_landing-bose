c9aad0f21e5aca7a7110a9c160222bcdb354d7a6
Alla Gutsul <allagutsul1990@gmail.com>
2023-11-17 15:49:22 +0200

    add animations to site

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+#Project "BOSE"
+
+This project is a web page created with HTML and CSS using the BEM methodology. The site adapts gracefully to various devices and screen sizes, ensuring a user-friendly interface whether accessed from a computer or a smartphone.
+
+## Project description
+
+This project is a single-page website that embodies an elegant and modern design for Bose products. Developed using contemporary web technologies, this site impresses not only with its stylish details but also with functionality that provides users with an unforgettable experience.
+
+## Technologies and tools
+
+The following technologies and tools were used in this project:
+
+### Clean HTML, CSS, JavaScript, and BEM Methodology:
+The project embraces clean and efficient code, leveraging the latest features of HTML5, CSS3, and JavaScript ES6+. Additionally, it follows the Block Element Modifier (BEM) methodology for a modular and maintainable structure.
+
+### Sass Preprocessor:
+I used Sass to make stylesheets more organized and easier to manage.
+
+### Google Fonts and Favicon:
+The implementation of the Inter font from Google Fonts enhances the visual appeal of text elements. The favicon contributes to branding, making the site easily recognizable in the browser's address bar.
+
+### GSAP Animations:
+Powered by the GSAP library, the site comes to life with scroll-triggered animations and dynamic property changes, delivering an interactive user experience.
+
+## Demo link
+
+You can view a live demo site at this link:  [BOSE](https://github.com/allagutsul27/layout_landing-bose)
+
+ ## Design Mockup
+
+To view the design mockup for this project, click on the following link: [Design Mockup](https://www.figma.com/file/DtkQmQ797hk0nI4KfMi2Uq/BOSE-New-Version?type=design&node-id=6703-88&mode=design&t=YdfQA62BsivNDe2O-0).
+
+## Launching the Project.
+
+1. Clone this repository to your computer.
+2. Use `npm install` or `yarn install` to install all project dependencies.
+3. Run the project with `npm start` or `yarn start`.
+4. Open a web browser and go to `http://localhost:3000` to see my application.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,162 @@
-'use strict';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+tl.fromTo(
+  '.header__title',
+  {
+    y: -200,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+  }
+).fromTo(
+  '.header__logo',
+  {
+    x: -50,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+  }
+).fromTo(
+  '.header__contact-us',
+  {
+    x: -50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.8,
+    stagger: 0.25,
+  }
+);
+
+gsap.from('.recommended__title', {
+  scrollTrigger: {
+    trigger: '.recommended',
+    start: '-200 0',
+  },
+  opacity: 0,
+  y: -50,
+  scale: 0.5,
+  duration: 1,
+  scrub: true,
+});
+
+gsap.from('.recommended__items', {
+  scrollTrigger: {
+    trigger: '.recommended',
+    start: '-200 0',
+  },
+  x: -100,
+  opacity: 0,
+  stagger: 0.5,
+  duration: 1,
+  scrub: true,
+});
+
+gsap.from('.categories__title', {
+  scrollTrigger: {
+    trigger: '.categories',
+    start: '-200 100',
+  },
+  x: -100,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from('.categories__subtitle', {
+  scrollTrigger: {
+    trigger: '.categories',
+    start: '-200 0',
+  },
+  duration: 1,
+  x: -100,
+  opacity: 0,
+});
+
+gsap.from('.categories__row', {
+  scrollTrigger: {
+    trigger: '.categories',
+    start: '-200 0',
+  },
+  duration: 0.8,
+  x: 250,
+  opacity: 0,
+  delay: 0.5,
+  stagger: 2,
+});
+
+gsap.from('.buy__title', {
+  scrollTrigger: {
+    trigger: '.buy',
+    start: '-300 0',
+  },
+  duration: 1,
+  y: -250,
+  opacity: 0,
+});
+
+gsap.from('.buy__benefits', {
+  scrollTrigger: {
+    trigger: '.buy',
+    start: '-300 0',
+  },
+  duration: 1,
+  x: 250,
+  opacity: 0,
+});
+
+gsap.from('.contacts__title', {
+  scrollTrigger: {
+    trigger: '.contacts',
+    start: '-300 0',
+  },
+  duration: 1,
+  x: 350,
+  y: -180,
+  opacity: 0,
+});
+
+gsap.from('.contacts__form', {
+  scrollTrigger: {
+    trigger: '.contacts',
+    start: '-200 0',
+  },
+  duration: 1,
+  x: -350,
+  opacity: 0,
+});
+
+gsap.from('.contacts__info', {
+  scrollTrigger: {
+    trigger: '.contacts',
+    start: '-200 0',
+  },
+  duration: 1,
+  x: 350,
+  opacity: 0,
+});
+
+gsap.from('.footer', {
+  scrollTrigger: {
+    trigger: 'footer',
+    start: '0 500',
+  },
+  duration: 1,
+  y: 250,
+  opacity: 0,
+});
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#nav') {
